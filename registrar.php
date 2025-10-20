0887cba521fdd087d0e40dfebb02625bbbc7be25
2023110511-cpu <2023110511@udh.edu.pe>
2025-10-20 15:38:15 -0500

    registrar.php

---
+++ registrar.php
@@ -0,0 +1,5 @@
+<form action="guardar.php" method="POST">
+    Nombre: <input type="text" name="nombre"><br>
+    Email: <input type="text" name="email"><br>
+    <input type="submit" value="Registrar">
+</form>
